e38ed0c5d5e7090320f528f3861a264528ee5377
chenp-fjnu <chp0524@gmail.com>
2017-02-02 22:00:57 +0800

    incldue NotORM php library

--- htdocs/non-wp/index.php
+++ htdocs/non-wp/index.php
@@ -20,17 +20,21 @@
 	              </thead>
 	              <tbody>
 	              <?php 
-				   include 'database.php';
-				   $pdo = Database::connect();
-				   $sql = $_SERVER["QUERY_STRING"]; //'SELECT * FROM wp_posts ORDER BY post_date DESC';
- 				   foreach ($pdo->query($sql) as $row) {
-					   		echo '<tr>';
-						   	echo '<td>'. $row['post_title'] . '</td>';
-						   	echo '<td>'. $row['post_content'] . '</td>';
-						   	echo '<td>'. $row['post_date'] . '</td>';
-						   	echo '</tr>';
-				   }
-				   Database::disconnect();
+				  			  include "NotORM.php";
+									$dbName = 'bdm247336490_db';
+									$dbHost = 'bdm247336490.my3w.com' ;
+									$dbUsername = 'bdm247336490';
+									$dbUserPassword = 'sql05247299';
+									$connection = new PDO( "mysql:host=".$dbHost.";"."dbname=".$dbName, $dbUsername, $dbUserPassword); 
+									$software = new NotORM($connection);
+
+									foreach ($software->wp_posts()->order('post_date_gmt DESC') as $row) { 
+										echo '<tr>';
+										echo '<td>'. $row['post_title'] . '</td>';
+										echo '<td>'. $row['post_content'] . '</td>';
+										echo '<td>'. $row['post_date'] . '</td>';
+										echo '</tr>';
+				   				}
 				  ?>
 			      </tbody>
             </table>
